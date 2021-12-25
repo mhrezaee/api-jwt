03f19d640fa8562dd0a8cc0ed17752ecc8e75324
Hadi <se.hadirezaee@gmail.com>
2021-12-25 11:46:09 +0100

    db models added

---
+++ MyWebApi/Models/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApi.Models
+{
+    public class MyDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}

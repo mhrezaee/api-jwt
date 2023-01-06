63430d31a82cd42c6f8e7887e040d1c794f66446
Hadi <se.hadirezaee@gmail.com>
2023-01-06 22:24:00 +0100

    fix typo

--- MyWebApi/Constants/ApplicationRoles.cs
+++ MyWebApi/Constants/ApplicationRoles.cs
@@ -4,5 +4,5 @@ public class ApplicationRoles
 {
     public const string Admin = "Admin";
     public const string User = "User";
-    // other roles here and also in an migration to update the databes roles
+    // other roles here and also in an migration to update the database roles
 }
